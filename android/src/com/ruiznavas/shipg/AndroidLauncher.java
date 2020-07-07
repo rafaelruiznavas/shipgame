1005cf5bc6231d0b7b4a480d63e72e5e46a80580
rafaelruiznavas <rafa@ruiznavas.com>
2020-07-07 19:40:18 +0200

    Initial commit

---
+++ android/src/com/ruiznavas/shipg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ruiznavas.shipg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ruiznavas.shipg.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
