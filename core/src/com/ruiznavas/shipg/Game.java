41dc6f72219d467a6af18892fa962bd30e395669
rafaelruiznavas <rafa@ruiznavas.com>
2020-07-08 13:52:26 +0200

    Entrada basica

--- core/src/com/ruiznavas/shipg/Game.java
+++ core/src/com/ruiznavas/shipg/Game.java
@@ -2,6 +2,7 @@ package com.ruiznavas.shipg;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x,y;
 	
 	@Override
 	public void create () {
@@ -20,8 +22,21 @@ public class Game extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			y+=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y-=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x-=4;
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x+=4;
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y); 
 		batch.end();
 	}
 	
