5d09383f8bfbf3f4950fca7422168753c4911f5d
rafaelruiznavas <rafa@ruiznavas.com>
2020-07-09 13:31:28 +0200

    Agregada animacion nave

--- core/src/com/ruiznavas/shipg/screens/MainGameScreen.java
+++ core/src/com/ruiznavas/shipg/screens/MainGameScreen.java
@@ -5,22 +5,39 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.ruiznavas.shipg.ShipGame;
 
 public class MainGameScreen implements Screen{
 	public static final float SPEED = 120;
-	Texture img;
+	public static final int ANCHO_PIXEL_SHIP = 16;
+	public static final int ALTO_PIXEL_SHIP = 24;
+	public static final int ANCHO_SHIP = ANCHO_PIXEL_SHIP * 3;
+	public static final int ALTO_SHIP = ALTO_PIXEL_SHIP * 3;
+	public static final float VELOCIDAD_ANIMACION_SHIP = 0.5f;
+	
+	Animation[] rolls; 
+	
 	float x,y;
+	int roll;
+	float stateTime;
 	
 	ShipGame game;
 	
 	public MainGameScreen(ShipGame game) {
 		this.game = game;
+		y = 15;
+		x = ShipGame.ANCHO/2 - ANCHO_SHIP / 2;
+		
+		roll = 1;
+		rolls = new Animation[5];
+		TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), ANCHO_PIXEL_SHIP,ALTO_PIXEL_SHIP);
+		rolls[roll] = new Animation<TextureRegion>(VELOCIDAD_ANIMACION_SHIP, rollSpriteSheet[0][2],rollSpriteSheet[1][2]);
 	}
 
 	@Override
 	public void show() {
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -40,11 +57,13 @@ public class MainGameScreen implements Screen{
 			x+= SPEED * delta;
 		}
 		
+		stateTime += delta;
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		game.getBatch().begin();
-		game.getBatch().draw(img, x, y); 
+		game.getBatch().draw((TextureRegion)rolls[roll].getKeyFrame(stateTime,true), x, y, ANCHO_SHIP, ALTO_SHIP); 
 		game.getBatch().end();
 	}
 
